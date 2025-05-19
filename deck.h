2b8b927d9e17b5fc1398fbc5a69718c9ca3c3ed9
Vincent Tran <33761211+VincentVinhTran@users.noreply.github.com>
2025-05-19 08:21:44 -0700

    Update deck.h

--- deck.h
+++ deck.h
@@ -1,7 +1,3 @@
-/*                           *\  
-    DO NOT MODIFY THIS FILE
-\*                           */
-
 #include <iostream>
 #include <ctime>
 #include <random>
@@ -17,15 +13,14 @@ const int MAX_CARDS = 52;
 
 class Deck {
     private:
-        // Vector to represent a deck of Cards
         vector<Card> cards;
-        // Helper function to compare Cards
         bool comesBefore(const Card&, const Card&);
         
 
     public:
         Deck();
         Deck(const Deck&);
+        Deck& operator=(const Deck&);
         ~Deck();
 
         bool isEmpty() const;
@@ -34,23 +29,10 @@ class Deck {
         Card draw();
         void addTop(const Card&);
         void shuffle();
+        void order();
+        int search(const Card&) const;
         void print(ostream&) const;
 
-        // ~~~ New Lab 12 Methods ~~~
-        /* Deck& operator=(const Deck&)
-            update assignment to return this object */
-        Deck& operator=(const Deck&);
-
-        /* void Deck::order()
-            sorts the deck in rank/suit order */
-        // Must implement using SELECTION SORT
-        void order();
-
-        /* int Deck::search(const Card&)
-            returns the index of the first occurence
-            of a Card in the deck */
-        // Must implement using BINARY SEARCH
-        int search(const Card&) const;
 };
 
 ostream& operator<<(ostream&, const Deck&);
